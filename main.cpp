a07e5a0fe7777ef991e29f75105117696c9a8ec2
Irakli <iraklimargve@gmail.com>
2022-01-10 17:15:21 +0400

    change23

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(){
   Home* ab = new Home(2,3);
   printf("%d\n", ab->Area());
   printf("%d\n", ab->Area());
+  printf("%d\n", ab->Area());
 
   // printf("%lld\n%lld\n", (long long int)&a, (long long int)b);
   return 0;
