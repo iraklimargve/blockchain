1b289753e855bedb7f37ee7202f9c077323e48f3
Irakli <iraklimargve@gmail.com>
2022-01-24 17:00:47 +0400

    add some example query for database

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 #include <iostream>
 #include <ctime>
 #include <string>
+#include <thread>
+#include <chrono>
 #include <pqxx/pqxx>
 
 #include "home.h"
 #include "config.h"
+
+struct cndedaia{
+  int id;
+  std::string name;
+  double salary;
+};
+
+void printcndedaia(cndedaia obj){
+  // printf("id: %d, name: %s, salary: %f\n", obj.id, obj.name, obj.salary);
+  std::cout << "id: " << obj.id << ", name: " << obj.name << ", salary: " << obj.salary << std::endl;
+};
 
 
 int main(){
@@ -23,6 +36,11 @@ int main(){
   printf("%s\n","Try connecting database");
 
   try {
+      cndedaia obj;
+      obj.name = "asdf";
+      obj.id = 5;
+      obj.salary = 12.23;
+      printcndedaia(obj);
       pqxx::connection C("dbname = postgres user = postgres password = password hostaddr = 127.0.0.1 port = 5432");
       printf("%s\n","checkeeeeeeeeeeers");
       if (C.is_open()) {
@@ -32,12 +50,24 @@ int main(){
          return 1;
       }
       pqxx::work W(C);
+      for (size_t i = 6; i < 10; i++) {
+        std::string q;
+        q = "insert into chenidedaiaaa(id, name, salary) values ";
+        q = q + "(" + std::to_string(i) + ", 'name" + std::to_string(i) + "', " + std::to_string(i * 10.0) + " )";
+        std::cout << q << std::endl;
+        W.exec(q);
+        W.commit();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+      }
 
-      // how to check if database exist??????????? chenidedaia
-      // if exist dont run this command if not run to create boishy
-      // W.exec("create table chenidedaiaaa(ID int primary key, name varchar(50), salary real);");
-      W.exec("insert into chenidedaiaaa(name, salary) values (1, 'chndedaiaboishy', 100000);");
+      pqxx::result res = W.exec("select id, name, salary from chenidedaiaaa");
       W.commit();
+      for (pqxx::row r : res) {
+        obj.id = r[0].as<int>();
+        obj.name = r[1].as<std::string>();
+        obj.salary = r[2].as<double>();
+        printcndedaia(obj);
+      }
       C.disconnect();
    } catch (const std::exception &e) {
       printf("%s\n",e.what());
